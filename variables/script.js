7fbffe640fc991ebe27e2c6c8e6844275cde6ab6
Abdifatah Ibrahim <codewithaftah@gmail.com>
2025-09-30 05:56:18 +0300

    added variables in js

---
+++ variables/script.js
@@ -0,0 +1,23 @@
+// there are three ways that we can declare a variable using var let and const
+
+// var: is a type of way we can define a variable in js but its outdated
+
+// var firstName = "Abdifatah";
+// var age = 27;
+
+// console.log(firstName);
+// console.log(age);
+
+// let this type of declaring a variable allows you to reassign your
+let myName = "Abdifatah Ibrahim";
+let myAge = 27;
+
+// console.log(myName);
+// console.log(myAge);
+
+// const we when we want to assign variable and we don't wanna change it later
+
+const firstName = "Abdifatah";
+const age = 27;
+console.log(typeof firstName, firstName);
+console.log(typeof age, age);
